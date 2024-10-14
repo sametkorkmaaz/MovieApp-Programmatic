eb20ea953f62d08d9794a2d430fb3f20cfc25e36
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2024-10-14 09:15:04 +0300

    Programmatic setup done

--- MovieApp-Programmatic/SplashViewController.swift
+++ MovieApp-Programmatic/SplashViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SplashViewController.swift
 //  MovieApp-Programmatic
 //
 //  Created by Samet Korkmaz on 14.10.2024.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashViewController: UIViewController {
 
+    // http://www.omdbapi.com/?s=recep&apikey=e4f84105
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
-
 }
-
