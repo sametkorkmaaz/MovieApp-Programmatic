5840ecbcc6970e6117967905dc9934143384099f
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2024-10-24 14:07:10 +0300

    comeback commit

--- MovieApp-Programmatic/Scene/Splash/SplashViewController.swift
+++ MovieApp-Programmatic/Scene/Splash/SplashViewController.swift
@@ -10,28 +10,78 @@ import UIKit
 protocol SplashViewInterface: AnyObject {
     
     func preparePage()
+    func splashTextAnimation(splashText: String?)
+    func presentSearchScreen()
+    func showNetworkError()
 }
 
 final class SplashViewController: UIViewController {
     // http://www.omdbapi.com/?s=recep&apikey=e4f84105
+    
+    private lazy var splashTextLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .loodos
+        label.font = .systemFont(ofSize: 30, weight: .semibold)
+        label.textAlignment = .center
+        return label
+    }()
+    
     private lazy var viewModel = SplashViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.view = self
         viewModel.viewDidLoad()
-    /*    MovieService().fetchMovies(movieTitle: "recep") { result in
-            switch result {
-            case .success(let movies):
-                print(movies)
-            case .failure(let error):
-                print(error)
-            }
-        } */
+        /*    MovieService().fetchMovies(movieTitle: "recep") { result in
+         switch result {
+         case .success(let movies):
+         print(movies)
+         case .failure(let error):
+         print(error)
+         }
+         } */
     }
 }
 
 extension SplashViewController: SplashViewInterface {
+    
     func preparePage() {
-        view.backgroundColor = .red
+        view.backgroundColor = .anaColor2
+        [splashTextLabel].forEach{
+            view.addSubview($0)
+        }
+        [splashTextLabel].forEach{
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            splashTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            splashTextLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-}
+    
+    func splashTextAnimation(splashText: String?) {
+        if let splashText = splashText {
+            DispatchQueue.main.async {
+                self.splashTextLabel.text = splashText
+            }
+        }
+    }
+    
+    func presentSearchScreen() {
+        let Search_VC = SearchViewController()
+        Search_VC.modalPresentationStyle = .fullScreen
+        present(Search_VC, animated: false, completion: nil)
+    }
+    
+    func showNetworkError() {
+        let alert = UIAlertController(title: "İnternet yok!", message: "Lütfen internet bağlantınızı kontrol ediniz.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Tamam", style: .default, handler: { _ in
+            self.viewModel.chechNetworkConnection()
+        }))
+        self.present(alert, animated: true, completion: nil)
+            
+        }
+    }
+
+    
+    
